95738989fcb06b9dc371d5d732ee91b9cec0c6ad
Harish Babu <harish_babu@outlook.com>
2022-11-05 00:57:55 +0000

    replace all property modifier with NextDay public method

--- src/GildedRose.Console/UpdateQualityService.cs
+++ src/GildedRose.Console/UpdateQualityService.cs
@@ -3,37 +3,11 @@ namespace GildedRose.Console;
 public abstract class Product
 {
     public string Name { get; }
-    public int SellIn { get; private set; }
-    public int Quality { get; private set; }
+    public int SellIn { get; }
+    public int Quality { get; }
 
-    protected Product(string name, int sellIn, int quality) =>
+    public Product(string name, int sellIn, int quality) =>
         (Name, SellIn, Quality) = (name, sellIn, quality);
-
-    public void IncreaseQuality()
-    {
-        if (Quality < 50)
-        {
-            Quality = Quality + 1;
-        }
-    }
-
-    public void DegradeQuality()
-    {
-        if (Quality > 0)
-        {
-            Quality = Quality - 1;
-        }
-    }
-
-    public void DropQuality()
-    {
-        Quality = 0;
-    }
-
-    public void ForwardDay()
-    {
-        SellIn = SellIn - 1;
-    }
 
     public abstract Product NextDay();
 
@@ -42,11 +16,11 @@ public abstract class Product
         switch (item.Name.ToLowerInvariant())
         {
             case "aged brie":
-                return new AgedBrieItem(item.Name, item.SellIn, item.Quality);
+                return new AgedBrie(item.Name, item.SellIn, item.Quality);
             case "backstage passes to a tafkal80etc concert":
-                return new BackstagePassItem(item.Name, item.SellIn, item.Quality);
+                return new BackstagePass(item.Name, item.SellIn, item.Quality);
             case "sulfuras, hand of ragnaros":
-                return new LegendaryItem(item.Name, item.SellIn, item.Quality);
+                return new LegendaryItem(item.Name, item.SellIn);
             default:
                 return new RegularItem(item.Name, item.SellIn, item.Quality);
         }
@@ -60,84 +34,62 @@ public sealed class RegularItem : Product
 
     public override Product NextDay()
     {
-        var result = new RegularItem(Name, SellIn, Quality);
-        result.DegradeQuality();
-        result.ForwardDay();
-        if (result.SellIn < 0)
+        var quality = SellIn switch
         {
-            result.DegradeQuality();
-        }
-        return result;
+            <= 0 => Quality - 2,
+            _ => Quality - 1
+        };
+        return new RegularItem(Name, SellIn - 1, Math.Min(Math.Max(0, quality), 50));
     }
 }
 
-public sealed class AgedBrieItem : Product
+public sealed class AgedBrie : Product
 {
-    public AgedBrieItem(string name, int sellIn, int quality) : base(name, sellIn, quality)
+    public AgedBrie(string name, int sellIn, int quality) : base(name, sellIn, quality)
     { }
 
     public override Product NextDay()
     {
-        var result = new AgedBrieItem(Name, SellIn, Quality);
-        result.IncreaseQuality();
-        result.ForwardDay();
-        if (result.SellIn < 0)
+        var quality = SellIn switch
         {
-            result.IncreaseQuality();
-        }
-        return result;
+            <= 0 => Quality + 2,
+            _ => Quality + 1
+        };
+        return new AgedBrie(Name, SellIn - 1, Math.Min(Math.Max(0, quality), 50));
     }
 }
 
-public sealed class BackstagePassItem : Product
+public sealed class BackstagePass : Product
 {
-    public BackstagePassItem(string name, int sellIn, int quality) : base(name, sellIn, quality)
+    public BackstagePass(string name, int sellIn, int quality) : base(name, sellIn, quality)
     { }
 
     public override Product NextDay()
     {
-        var result = new BackstagePassItem(Name, SellIn, Quality);
-        result.IncreaseQuality();
-        if (result.SellIn <= 10)
+        int quality = SellIn switch
         {
-            result.IncreaseQuality();
-        }
-
-        if (result.SellIn <= 5)
-        {
-            result.IncreaseQuality();
-        }
-        result.ForwardDay();
-
-        if (result.SellIn < 0)
-        {
-            result.DropQuality();
-        }
-        return result;
+            <= 0 => 0,
+            <= 5 => Quality + 3,
+            <= 10 => Quality + 2,
+            _ => Quality + 1
+        };
+        return new BackstagePass(Name, SellIn - 1, Math.Min(Math.Max(0, quality), 50));
     }
 }
 
 public sealed class LegendaryItem : Product
 {
-    public LegendaryItem(string name, int sellIn, int quality) : base(name, sellIn, quality)
+    public LegendaryItem(string name, int sellIn) : base(name, sellIn, 80)
     { }
 
     public override Product NextDay() =>
-        new LegendaryItem(Name, SellIn, Quality);
+        new LegendaryItem(Name, SellIn);
 }
 
 public class UpdateQualityService
 {
-    public IEnumerable<Item> Update(IEnumerable<Item> items)
-    {
-        return items
-            .Select(Product.Create)
-            .Select(x => UpdateItem(x));
-    }
-
-    private static Item UpdateItem(Product item)
-    {
-        var itemNextDay = item.NextDay();
-        return new Item { Name = itemNextDay.Name, SellIn = itemNextDay.SellIn, Quality = itemNextDay.Quality };
-    }
+    public IEnumerable<Item> Update(IEnumerable<Item> items) =>
+        items.Select(Product.Create)
+            .Select(x => x.NextDay())
+            .Select(x => new Item { Name = x.Name, SellIn = x.SellIn, Quality = x.Quality });
 }
